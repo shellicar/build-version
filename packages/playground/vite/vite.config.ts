bb0a50a15565247051ac4b2dd06f78c7a40a8abd
Stephen Hellicar <shellicar@gmail.com>
2025-01-05 04:41:42 +1000

    Test

--- packages/playground/vite/vite.config.ts
+++ packages/playground/vite/vite.config.ts
@@ -3,6 +3,12 @@ import VersionPlugin from '@shellicar/build-version/vite';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 
+console.log('env', Object.entries(process.env));
+
+const isGithub = process.env['GITHUB_ACTIONS'];
+const isCi = process.env['CI'];
+console.log('test', { isGithub, isCi });
+
 const options: Options = {
   debug: true,
   versionCalculator: 'gitversion',
