e190367d9d534c0a358ae8131dac6432d3d51825
Stephen Hellicar <shellicar@gmail.com>
2025-01-05 21:38:33 +1000

    Properly escape

--- packages/@shellicar/build-version/src/core/module.ts
+++ packages/@shellicar/build-version/src/core/module.ts
@@ -3,4 +3,4 @@ import { name } from '../../package.json';
 const exportSpecifier = 'version';
 
 export const MODULE_ID = `${name}/${exportSpecifier}`;
-export const versionPath = `${name}/dist/core/${exportSpecifier}\.c?js`;
+export const versionPath = `${name}/dist/core/${exportSpecifier}\\.c?js`;
