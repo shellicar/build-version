87c619ea61c4890b30586d15640fbaf3d85eff09
Stephen Hellicar <shellicar@gmail.com>
2025-01-05 21:36:10 +1000

    Fix more paths

--- packages/playground/vite/vite.config.ts
+++ packages/playground/vite/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
-import type { Options } from '../../@shellicar/build-version/dist/core/types';
-import VersionPlugin from '../../@shellicar/build-version/dist/vite';
+import type { Options } from '@shellicar/build-version/types';
+import VersionPlugin from '@shellicar/build-version/vite';
 
 const versionCalculator = process.env.CI ? 'git' : 'gitversion';
 
