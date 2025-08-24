da7633a681f77c4c9ae35d71fcfbdf20ed108ddf
Stephen Hellicar <shellicar@gmail.com>
2025-08-24 21:02:57 +1000

    Update with latest scripts. Update packages. Fix vulnerabilities.

---
+++ examples/esm/tsup.config.ts
@@ -0,0 +1,29 @@
+import versionPlugin from '@shellicar/build-version/esbuild';
+import type { Options } from '@shellicar/build-version/types';
+import { defineConfig } from 'tsup';
+
+const pluginOptions = {
+  debug: true,
+  strict: true,
+  versionCalculator: () => ({
+    branch: 'bob',
+    version: '1.2.3',
+  }),
+} satisfies Options;
+
+export default defineConfig(() => ({
+  entry: ['src/main.ts'],
+  splitting: true,
+  sourcemap: false,
+  treeshake: true,
+  dts: false,
+  clean: false,
+  minify: false,
+  keepNames: true,
+  bundle: true,
+  tsconfig: 'tsconfig.json',
+  target: 'node22',
+  format: ['esm'],
+  outDir: 'dist',
+  esbuildPlugins: [versionPlugin(pluginOptions)],
+}));
--- packages/build-version/tsup.config.ts
+++ packages/build-version/tsup.config.ts
@@ -1,17 +1,31 @@
-import type { Options } from 'tsup';
+import cleanPlugin from '@shellicar/build-clean/esbuild';
+import { defineConfig, type Options } from 'tsup';
 
-export default (<Options>{
-  entry: ['src/**/*.ts'],
-  clean: true,
-  bundle: true,
-  format: ['cjs', 'esm'],
-  tsconfig: 'tsconfig',
-  dts: true,
-  cjsInterop: true,
-  treeshake: true,
-  outDir: 'dist',
-  minify: 'terser',
-  sourcemap: true,
-  keepNames: true,
-  splitting: true,
-});
+const commonOptions = (watch: Options['watch']) =>
+  ({
+    entry: ['src/**/*.ts'],
+    clean: false,
+    bundle: true,
+    tsconfig: 'tsconfig.json',
+    dts: true,
+    cjsInterop: true,
+    treeshake: true,
+    minify: watch ? 'terser' : false,
+    sourcemap: true,
+    keepNames: true,
+    splitting: true,
+    esbuildPlugins: [cleanPlugin({ destructive: true })],
+  }) satisfies Options;
+
+export default defineConfig((options) => [
+  {
+    ...commonOptions(options.watch),
+    format: ['esm'],
+    outDir: 'dist/esm',
+  },
+  {
+    ...commonOptions(options.watch),
+    format: ['cjs'],
+    outDir: 'dist/cjs',
+  },
+]);
